3c4be0a69feee84e0d5fbe7360d9afb78a274ac9
JulioSpassky <juliocto2015@gmail.com>
2021-05-22 21:36:39 -0300

    :tada: Initial Commit

---
+++ costumer-ms/src/test/java/com/juliospassky/costumerms/CostumerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juliospassky.costumerms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostumerMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
